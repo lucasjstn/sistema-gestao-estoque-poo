a763e66dd9d38ff0dd9aee248a5c218a65fec0a5
Lucas Oliveira <72674138+lucasjstn@users.noreply.github.com>
2023-11-16 15:24:30 -0300

    feat: criado a classe produto

--- src/test/java/com/main/ProdutoTeste.java
+++ src/test/java/com/main/ProdutoTeste.java
@@ -1,5 +1,14 @@
 package com.main;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ProdutoTeste {
+
+    @Test
+    void mostrarProduto() {
+        
+    }
     
 }
